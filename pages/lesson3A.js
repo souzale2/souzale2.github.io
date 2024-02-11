1b7df8261f798d2ecb04b1f2cd21b33388774ee5
Patrick Lemos <patrick.lemos@vollrathco.com>
2024-02-11 13:29:19 -0600

    Working lesson 3. Needs cleaning

---
+++ pages/lesson3A.js
@@ -0,0 +1,237 @@
+
+var objectGiff  = {
+    "revelação em camadas": [
+        {
+            "url": "/images/camadas-da-terra-1181453587.jpg",
+            "caption": "Revelacao em Camadas. O melhor jeito de aprender. Do simples ate o complexo."
+        },
+        {
+            "url": "/images/bedSheets.jpg",
+            "caption": "Tudo que e bom tem camads. Hamburgers, Pizza, technologia. Exepto lencois. Paia demais."
+        }
+    ],
+    "israel": [
+        {
+            "url": "/images/Israel.jpg",
+            "caption": "Israel. Onde tudo comecou. Esquece do Jardin do Eden por agora."
+        },
+        {
+            "url": "/images/IsraelGuerra.jpg",
+            "caption": "ISRAEL. Nao a Guerra em ISRAEL"
+        }
+    ],
+    "Pai": [
+        {
+            "url": "/images/",
+            "caption": ""
+        },
+        {
+            "url": "/images/",
+            "caption": ""
+        }
+    ],
+    "fiéis": [
+        {
+            "url": "/images/Bolconario.jpg",
+            "caption": "Pai. Iqual Pai Bolconario."
+        },
+        {
+            "url": "/images/lula.jpg",
+            "caption": "Tem fruto nenhum de Pai."
+        }
+    ],
+    "terra prometida": [
+        {
+            "url": "/images/TerraPrometida.jpg",
+            "caption": "Bora para a Terra Prometida."
+        },
+        {
+            "url": "/images/TerraDespromedita.jpg",
+            "caption": "Terra que o diabo prometeu."
+        }
+    ],
+    "Semente": [
+        {
+            "url": "/images/semente.jpg",
+            "caption": "E fijao? E cafe? NAO! E semente"
+        },
+        {
+            "url": "/images/oldTree.jpg",
+            "caption": "Talvez e velho iqual abraao. Porque, concerteza nao e a semente dele."
+        }
+    ],
+    "Jesus": [
+        {
+            "url": "",
+            "caption": "Êxodo 20:4-6 (NVI): Não farás para ti imagem de escultura, nem semelhança alguma"
+        },
+        {
+            "url": "",
+            "caption": "Voce errou soletrar Jesus. Que vergonha!"
+        }
+    ],
+    "em fé": [
+        {
+            "url": "/images/faithEmogi.jpg",
+            "caption": "em fe. Hallelujah!"
+        },
+        {
+            "url": "/images/semFe.png",
+            "caption": "Sem fe. Isso nao e Dedo do meio. E Serio! "
+        }
+    ],
+    "Do jeito deles": [
+        {
+            "url": "/images/DoJeitoDeles.jpg",
+            "caption": "Do jeito deles. Faz de qualquer maneira. Pra vc ver"
+        },
+        {
+            "url": "/images/CaraNaValca.jpg",
+            "caption": "Vc soletrou errado. Vc merece ver isso."
+        }
+    ],
+    "Ismael": [
+        {
+            "url": "/images/ismael.jpg",
+            "caption": "Toda vez que pensate na aparnecia de Ismael, pense nisso."
+        },
+        {
+            "url": "/images/PapaiVerdel.jpg",
+            "caption": "E Ismael. Nao Papai Verdel"
+        }
+    ],
+    "muçulmanos": [
+        {
+            "url": "/images/OilBarrel.jpg",
+            "caption": "Muito Bem. Soletrou Muçulmanos Correto!"
+        },
+        {
+            "url": "/images/UrsoHumano.jpg",
+            "caption": "E Muçulmanos. Nao Urso Humano."
+        }
+    ],
+    "Israelitas": [
+        {
+            "url": "/images/IsraelitasMana.jpg",
+            "caption": "Estreando: Israelitas catando Mana. Viras no futuro episodio."
+        },
+        {
+            "url": "/images/KereaDoNorte.jpg",
+            "caption": "Que isso! Israelitas da Korea do Norte? Vai dormir!"
+        }
+    ],
+    "Primario": [
+        {
+            "url": "",
+            "caption": "Primario! Soletrou correto. Parabems?"
+        },
+        {
+            "url": "/images/primario.jpg",
+            "caption": "Vai de volta para o creche."
+        }
+    ],
+    "Secundario": [
+        {
+            "url": "/images/",
+            "caption": "Secundario! Soletrou correto. E ba! 👏"
+        },
+        {
+            "url": "/images/primario.jpg",
+            "caption": "Vai de volta para o creche."
+        }
+    ],
+    "carne": [
+        {
+            "url": "/images/carne.jpg",
+            "caption": "Carne"
+        },
+        {
+            "url": "/images/TalvezCarne.jpg",
+            "caption": "Talvez Carne? O que vc acha?"
+        }
+    ],
+    "espírito": [
+        {
+            "url": "/images/",
+            "caption": "espirito. Re-le exodus 20:4-6"
+        },
+        {
+            "url": "/images/",
+            "caption": "Errou espirito. Que a verdade. Eu tambem erraria."
+        }
+    ],
+    "Abraão": [
+        {
+            "url": "/images/Abraham.jpg",
+            "caption": "E Abrao. Nao Abraham."
+        },
+        {
+            "url": "/images/Abraham.jpg",
+            "caption": "Perto. Mas E Abraão. Nao Abrão"
+        }
+    ],
+    "circuncisão": [
+        {
+            "url": "/images/",
+            "caption": "A circuncisão e muito grafico. Somente o perderdor ira ter esta imagem."
+        },
+        {
+            "url": "/images/BloodyScissors.jpg",
+            "caption": "Vc nao quer saber o que aconteceu aqui."
+        }
+    ],
+    "coração": [
+        {
+            "url": "/images/coracao.jpg",
+            "caption": "love    Desculpa   coracao"
+        },
+        {
+            "url": "/images/rim.jpg",
+            "caption": "Se coracao e soletrado de qualquer jeito. Vamos mostralo de qualquer jeito tambem."
+        }
+    ],
+    "Fiél": [
+        {
+            "url": "/images/",
+            "caption": "Fiel. Bom Trabalho"
+        },
+        {
+            "url": "/images/",
+            "caption": "Fiel. Mal Trabalho"
+        }
+    ],
+    "Testou": [
+        {
+            "url": "/images/Test.jpg",
+            "caption": "Attaque de anciedade vindo em 3.. 2.. 1.."
+        },
+        {
+            "url": "/images/abc.jpg",
+            "caption": "B com E, Be, Be com sta, Besta."
+        }
+    ],
+    "pequenos altares": [
+        {
+            "url": "/images/PequenasBatalhas.jpg",
+            "caption": "Batalhas pequenas. Igual Horando por coisas que parecem ser insignificantes. Mas Deus ainda responde"
+        },
+        {
+            "url": "/images/ArvoreDoAmor.jpg",
+            "caption": "Vc e Amado, mesmo que nao saiba suletrar."
+        }
+    ],
+    "do cordeiro de Deus": [
+        {
+            "url": "/images/Cordeiro.jpg",
+            "caption": "O Cordeiro mais fofo da internet!"
+        },
+        {
+            "url": "/images/CordeiroFrito.jpg",
+            "caption": "Sim. E cordeiro. Mas nao o que eu estava pensando."
+        }
+    ]
+};
+
+
+
+  
